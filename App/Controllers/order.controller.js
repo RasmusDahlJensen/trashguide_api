d853fd4c9712e51ee13d65506d12b84bb53ebe76
Rasmus Dahl Jensen <62021203+RasmusDahlJensen@users.noreply.github.com>
2023-09-22 00:34:20 +0200

    Added remove endpoint

--- App/Controllers/order.controller.js
+++ App/Controllers/order.controller.js
@@ -1,54 +1,54 @@
-import sequelize from "../../Config/sequelize.config.js"
-import Orders from "../Models/order.model.js"
-import { Sequelize } from "sequelize"
+import sequelize from "../../Config/sequelize.config.js";
+import Orders from "../Models/order.model.js";
+import { Sequelize } from "sequelize";
 
 /**
  * Controller foc City Actions
  */
 class OrderController {
-  /**
-   * Method List
-   * @param {Object} req Express Request Object
-   * @param {Object} res Express Response Object
-   */
-  list = async (req, res) => {
-    // Destructure Assignment - optional list management
-    let { sortkey, sortdir, limit, attributes } = req.query
-    // Sætter array til sort og retning
-    const order = [sortkey ? sortkey : "id"]
-    order.push(sortdir || "ASC")
-    // Sætter limit antal
-    limit = parseInt(limit) || 1000
-    // Sætter attributter (table felter)
-    const attr = attributes
-      ? attributes.split(",")
-      : new Array("id", "fullname", "email")
+	/**
+	 * Method List
+	 * @param {Object} req Express Request Object
+	 * @param {Object} res Express Response Object
+	 */
+	list = async (req, res) => {
+		// Destructure Assignment - optional list management
+		let { sortkey, sortdir, limit, attributes } = req.query;
+		// Sætter array til sort og retning
+		const order = [sortkey ? sortkey : "id"];
+		order.push(sortdir || "ASC");
+		// Sætter limit antal
+		limit = parseInt(limit) || 1000;
+		// Sætter attributter (table felter)
+		const attr = attributes
+			? attributes.split(",")
+			: new Array("id", "fullname", "email");
 
-    // Eksekverer sequelize metode med management values
-    const result = await Orders.findAll({
-      attributes: attr,
-      order: [order],
-      limit: limit
-    })
-    // Udskriver resultat i json format
-    res.json(result)
-  }
+		// Eksekverer sequelize metode med management values
+		const result = await Orders.findAll({
+			attributes: attr,
+			order: [order],
+			limit: limit,
+		});
+		// Udskriver resultat i json format
+		res.json(result);
+	};
 
-  /**
-   * Method Details
-   * @param {Object} req Express Request Object
-   * @param {Object} res Express Response Object
-   */
-  details = async (req, res) => {
-    // Destructure assignment af id.
-    const { id } = req.params || 0
-    // Eksekverer sequelize metode med attributter og where clause
-    const result = await Orders.findOne({
-      where: { id: id }
-    })
-    // Udskriver resultat i json format
-    res.json(result)
-  }
+	/**
+	 * Method Details
+	 * @param {Object} req Express Request Object
+	 * @param {Object} res Express Response Object
+	 */
+	details = async (req, res) => {
+		// Destructure assignment af id.
+		const { id } = req.params || 0;
+		// Eksekverer sequelize metode med attributter og where clause
+		const result = await Orders.findOne({
+			where: { id: id },
+		});
+		// Udskriver resultat i json format
+		res.json(result);
+	};
 
 	/**
 	 * Create Metode - opretter ny record
@@ -56,27 +56,60 @@ class OrderController {
 	 * @param {object} res Response Object
 	 * @return {number} Returnerer nyt id
 	 */
-  create = async (req, res) => {
-		const { fullname, address, zipcode, city, email, phone } = req.body
+	create = async (req, res) => {
+		const { fullname, address, zipcode, city, email, phone } = req.body;
 
-		if(fullname && address && zipcode && city && email) {
+		if (fullname && address && zipcode && city && email) {
 			try {
-				const model = await Orders.create(req.body)
+				const model = await Orders.create(req.body);
 				return res.json({
 					message: `Record created`,
-					newId: model.id
-				})					
+					newId: model.id,
+				});
 			} catch (error) {
 				res.status(418).send({
-					message: `Could not create record: ${error}`
-				})									
+					message: `Could not create record: ${error}`,
+				});
 			}
 		} else {
 			res.status(403).send({
-				message: `Wrong parameter values`
-			})					
+				message: `Wrong parameter values`,
+			});
 		}
-	}  
+	};
+
+	/**
+	 * Delete Method - removes a record by ID
+	 * @param {object} req Request Object
+	 * @param {object} res Response Object
+	 */
+	remove = async (req, res) => {
+		const { id } = req.params;
+
+		try {
+			// Attempt to find the order by ID and delete it
+			const deletedOrder = await Orders.destroy({
+				where: {
+					id: id,
+				},
+			});
+
+			if (deletedOrder) {
+				return res.json({
+					message: `Order with ID ${id} deleted successfully.`,
+				});
+			} else {
+				return res.status(404).json({
+					message: `Order with ID ${id} not found.`,
+				});
+			}
+		} catch (error) {
+			console.error(error);
+			return res.status(500).json({
+				message: `Could not delete order: ${error.message}`,
+			});
+		}
+	};
 }
 
-export default OrderController
+export default OrderController;
